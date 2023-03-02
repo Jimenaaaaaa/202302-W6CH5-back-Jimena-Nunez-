ef23b666e32448cabfb521af47259ba4941669ee
Jimena <jimenanrp2@gmail.com>
2023-03-02 23:18:18 +0100

    Instance new usersMongoRepo

--- src/routers/dogs.router.ts
+++ src/routers/dogs.router.ts
@@ -2,11 +2,14 @@
 import { Router } from 'express';
 import { DogsController } from '../controllers/dogs.controllers.js';
 import { DogsMongoRepo } from '../repo/dogs.mongo.repo.js';
+import { UsersMongoRepo } from '../repo/user.mongo.repo.js';
 
 export const dogsRouter = Router();
 
 const repo = new DogsMongoRepo();
-const controller = new DogsController(repo);
+const userRepo = new UsersMongoRepo()
+
+const controller = new DogsController(repo, userRepo);
 
 dogsRouter.get('/', controller.getAll.bind(controller));
 dogsRouter.get('/:id', controller.get.bind(controller));
